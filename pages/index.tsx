8b06feaa174287b42b5c8edaf02e9d2b5dceaa38
Marcel Draeger <marcel.draeger@icloud.com>
2022-07-17 18:34:59 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,3 @@
-import 'swiper/css';
-import 'swiper/css/pagination';
-
 import type { NextPage } from 'next';
 import { useEffect, useRef } from 'react';
 
@@ -12,28 +9,37 @@ const Home: NextPage = () => {
   }, [vidRef]);
 
   return (
-    <div>
+    <>
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
       <video
+        className={'bg-video'}
         autoPlay
-        loop
         muted
+        loop
         playsInline
-        className={'sm:object-left'}
         ref={vidRef}
       >
-        <source src="/video/bg-video.mov" type="video/mp4" />
+        <source src="/video/video-3.mp4" type="video/mp4" />
       </video>
 
-      <div className="container mx-auto">
-        <div className="flex h-screen">
-          <div className="m-auto text-wrapper">
-            <h2>17.08.2022</h2>
-            <h1>Wir Heiraten</h1>
-          </div>
+      <div className="view">
+        <div className="main-text-wrapper">
+          <h2 style={{ paddingBottom: '8px' }}>Jane & Marcel</h2>
+          <h1>Heiraten</h1>
+          <h2 style={{ paddingTop: '8px' }}>am 17.08.2022</h2>
         </div>
       </div>
-    </div>
+      <div className="view">
+        <div className="card">
+          <h1>Bitte final zusagen!</h1>
+          <p>
+            ...um die letzten Schritte mit der Location, Deko und allem drum
+            herum zu finalisieren, bitten wir euch, uns einmal beschheid zu
+            geben persönlich, Telefon, whatsapp.
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
