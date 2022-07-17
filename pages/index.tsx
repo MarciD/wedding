eb1154ca4c33db79f0878d2fd892a2ed4865484e
Marcel Draeger <marcel.draeger@icloud.com>
2022-07-17 11:26:52 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,26 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 import type { NextPage } from 'next';
+import { useEffect, useRef } from 'react';
 
 const Home: NextPage = () => {
+  const vidRef = useRef<HTMLVideoElement>();
+
+  useEffect(() => {
+    vidRef?.current?.play();
+  }, [vidRef]);
+
   return (
     <div>
       {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
-      <video autoPlay loop muted playsInline className={'sm:object-left'}>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className={'sm:object-left'}
+        ref={vidRef}
+      >
         <source src="/video/bg-video.mov" type="video/mp4" />
       </video>
 
